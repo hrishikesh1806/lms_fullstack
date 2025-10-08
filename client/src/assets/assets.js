73da5d839ea25d490af415648b6ee6307a050a84
hrishikesh1806 <hrishikesh.aparaitech@gmail.com>
2025-10-08 13:12:55 +0530

    Final fix: Changed public folder asset import to URL string variable

--- client/src/assets/assets.js
+++ client/src/assets/assets.js
@@ -7,8 +7,8 @@ import upload_area from './upload_area.svg'
 import sketch from './sktech.svg' 
 import heroVideo from './herobg.mp4' 
 import microsoft_logo from './microsoft_logo.svg'
-import CallToActionVideo from '/CallToActionVid.mp4'; 
-import companiesVideo from './CompaniesBg.mp4' // <--- NEW IMPORT ADDED
+const CallToActionVideo = '/CallToActionVid.mp4'; 
+const companiesVideo = '/CompaniesVid.mp4' // <--- NEW IMPORT ADDED
 import walmart_logo from './walmart_logo.svg'
 import CourseSection_bg from './CourseSection.jpg';
 import accenture_logo from './accenture_logo.svg'
